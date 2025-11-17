c2c64449c44438e8b83394a39ed57b7d061beb20
Braedon Klock <kloc0004@algonquinlive.com>
2025-11-16 20:43:30 -0500

    created ScrollToTop.jsx helper in the helpers folder to scroll to the top of the page on each route change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import TransactionTree from "./pages/TransactionTreePage";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 function App() {
 
   return (
     <>
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/transaction-history" element={<TransactionTree/>} />
